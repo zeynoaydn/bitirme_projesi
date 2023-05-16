83193e9f148ef4bbad3295e11d85c3992516ab7f
Zeynep Aydın <zeynoaydnn@gmail.com>
2023-05-16 11:29:57 +0300

    a

--- mobil/lib/page_to_edit.dart
+++ mobil/lib/page_to_edit.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:gallery_saver/gallery_saver.dart';
-import 'package:http/http.dart' as http;
-import 'package:dio/dio.dart';
 
 class PageToEdit extends StatefulWidget {
   PageToEdit({
